ab4aaf7558145e0c8323d4bf852028a6a4c1792b
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-12-31 09:59:47 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import ProjectsSection from './components/ProjectsSection';
-import TechBanner from './components/TechBanner';
-import Achievements from './components/Achievements';
-import SocialSection from './components/SocialSection';
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import ProjectsSection from "./components/ProjectsSection";
+import TechBanner from "./components/TechBanner";
+import Achievements from "./components/Achievements";
+import SocialSection from "./components/SocialSection";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <TechBanner />
       <Achievements />
       <SocialSection />
+      <Analytics />
     </main>
   );
 }
