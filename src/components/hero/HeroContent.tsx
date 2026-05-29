933b86e317535c77757f0b12c03513f7f9bff8d6
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-05-29 18:08:29 +0530

    some  pixels fixed :)

--- src/components/hero/HeroContent.tsx
+++ src/components/hero/HeroContent.tsx
@@ -1,7 +1,7 @@
 export default function HeroContent() {
   return (
     <div className="mx-auto max-w-4xl text-center">
-      <p className="text-lg leading-9 text-gray-300 md:text-xl">
+      <p className="text-4xl leading-9 text-gray-400 md:text-2xl">
         こんにちは  I am Divyanshu Shekhar. I love weird hobbies, anime, writing, reading lots of books and sketching sometimes. Besides, I am also a programmer. I love to fiddle with databases and design elegant pixels.
         
         <br/>
