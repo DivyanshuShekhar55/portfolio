2c72bed11bcf64c532a05826c795c6a69cb01e64
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-05-29 17:44:18 +0530

    final changes for now

--- src/components/hero/HeroContent.tsx
+++ src/components/hero/HeroContent.tsx
@@ -2,7 +2,7 @@ export default function HeroContent() {
   return (
     <div className="mx-auto max-w-4xl text-center">
       <p className="text-lg leading-9 text-gray-300 md:text-xl">
-        こんにちは  I am Divyanshu Shekhar. I love weird hobbies, anime, writing, reading lots of books and sketching sometimes. Besides I am also a programmer. I love to fiddle with Database internals and design elegant pixels.
+        こんにちは  I am Divyanshu Shekhar. I love weird hobbies, anime, writing, reading lots of books and sketching sometimes. Besides, I am also a programmer. I love to fiddle with databases and design elegant pixels.
         
         <br/>
         <br/>
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,54 +1,68 @@
-import { Project } from '../types/project';
-import ChatApp from "../assests/ChatApp.png"
-import flappy from "../assests/flap-py.png"
-import screen from "../assests/screen-ai.png"
-import server from "../assests/server.png"
-import tinder from "../assests/tinder.png"
+import { Project } from "../types/project";
+import ChatApp from "../assests/ChatApp.png";
+import flappy from "../assests/flap-py.png";
+import server from "../assests/server.png";
+import tinder from "../assests/tinder.png";
+import catfish from "../assests/catfish.png";
+import stocks from "../assests/stocks.png";
 
 export const projects: Project[] = [
   {
     id: 1,
     number: "01",
-    title: "Chat App",
-    description: "Made a Scalable Chat App backend with pub-sub architecture and load balancing.",
-    image: ChatApp,
-    category: "Golang • CassandraDB • Redis • HAProxy",
-    href:"https://github.com/DivyanshuShekhar55/go-cassandra"
+    title: "Catfish",
+    description:
+      "A Postgres connection pooler, based on Figma's PGKeeper paper. Multi-threaded, Load Management, Observability",
+    image: catfish,
+    category: "Golang • Postgres •  DockerHub",
+    href: "https://github.com/DivyanshuShekhar55/catfish",
   },
   {
     id: 2,
     number: "02",
     title: "Tinder Search Algorithm",
-    description: "Replicated a tiny version of Tinder's Location search algorithm ",
+    description:
+      "Replicated a tiny version of Tinder's Location search algorithm ",
     image: tinder,
     category: "Elasticsearch • Golang • Postgres • React Native",
-    href:"https://github.com/DivyanshuShekhar55/yellow-monkey"
+    href: "https://github.com/DivyanshuShekhar55/yellow-monkey",
   },
   {
     id: 3,
     number: "03",
-    title: "Flappy Pushups",
-    description: "Flappy Bird game ... But you play while doing pushups. ",
-    image: flappy,
-    category: "PyGame • Computer Vision",
-    href:"https://github.com/DivyanshuShekhar55/flap-py"
+    title: "rn-cute-stocks",
+    description:
+      "A npm package for ReactNative apps. Provides high speed and elegant charts using  Skia and D3.js",
+    image: stocks,
+    category: "React Native • Animation •  npm",
+    href: "https://github.com/DivyanshuShekhar55/rn-cute-stocks",
   },
   {
     id: 4,
     number: "04",
-    title: "Http Server",
-    description: "Http Server created from scratch in Golang",
-    image: server,
-    category: "Golang",
-    href:"https://github.com/DivyanshuShekhar55/go-httpx"
+    title: "Chat App",
+    description:
+      "Made a Scalable Chat App backend with pub-sub architecture and load balancing.",
+    image: ChatApp,
+    category: "Golang • CassandraDB • Redis • HAProxy",
+    href: "https://github.com/DivyanshuShekhar55/go-cassandra",
   },
   {
     id: 5,
     number: "05",
-    title: "Lets Go",
-    description: "Repo with lots of examples and programs to master programming in Go. Also includes a bunch of great resources. ",
-    image: "https://images.unsplash.com/photo-1561070791-2526d30994b5?auto=format&fit=crop&q=80",
+    title: "Flappy Pushups",
+    description: "Flappy Bird game ... But you play while doing pushups. ",
+    image: flappy,
+    category: "PyGame • Computer Vision",
+    href: "https://github.com/DivyanshuShekhar55/flap-py",
+  },
+  {
+    id: 6,
+    number: "06",
+    title: "Http Server",
+    description: "Http Server created from scratch in Golang",
+    image: server,
     category: "Golang",
-    href:"https://github.com/DivyanshuShekhar55/Lets-Go"
-  }
+    href: "https://github.com/DivyanshuShekhar55/go-httpx",
+  },
 ];
